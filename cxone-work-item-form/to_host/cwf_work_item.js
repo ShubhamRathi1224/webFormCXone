eb7ee9a3bc5bdd16b320d6346c385da3446b56d9
Shubham Rathi <shubham.rathi@xebia.com>
2025-12-17 00:24:05 +0530

    feat: add code fix change

--- cxone-work-item-form/to_host/cwf_work_item.js
+++ cxone-work-item-form/to_host/cwf_work_item.js
@@ -157,7 +157,7 @@ window.onload = async () => {
 
   await loadData();
   initializeTable();
-  if(document.getElementById('wi_needUserDetails').value === 1){
+  if(document.getElementById('wi_needUserDetails').value == 1){
     document.getElementById('updateEmployeeBtn').style.display = 'block';
   } else {
     document.getElementById('updateEmployeeBtn').style.display = 'none';
